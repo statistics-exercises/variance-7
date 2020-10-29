f019bb38b35ff3e284b691a96b808da4c5cbc1bf
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-29 19:55:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+myvar = np.random.uniform(0,1)
+ssum, ssum2 = myvar, myvar*myvar 
+indices, S2 = np.zeros(200), np.zeros(200)
+for i in range(200) : 
+  # Add code to setup the numpy arrays called indices and average to generate the desired plot here.
+  
+  
+  
+# This will plot the graph for the data.  You should not need to adjust this.
+plt.plot( indices, S2, 'ro' )
+plt.xlabel("Number of random variables")
+plt.ylabel("S^2")
+plt.savefig("variance.png")
