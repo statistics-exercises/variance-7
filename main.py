3b75a961261dfdde9801693a6ca89e362205af47
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-14 14:56:07 +0100

    Added answers

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ ssum, ssum2 = myvar, myvar*myvar
 indices, S2 = np.zeros(200), np.zeros(200)
 for i in range(200) : 
   # Add code to setup the numpy arrays called indices and average to generate the desired plot here.
-  
+  indices[i] = i+2
+  myvar = np.random.uniform(0,1)
+  ssum, ssum2 = ssum + myvar, ssum + myvar*myvar 
+  mean = ssum / indices[i] 
+  S2[i] = (indices[i] / (indices[i]-1))*( ssum2 / indices[i] - mean*mean )
+
+plt.plot( indices, S2, 'ro' )
+plt.xlabel("Number of random variables")
+plt.ylabel("Sample variance")
+plt.savefig("variance.png") 
