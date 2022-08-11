595e41d3f8ebfc4953e16808e98f509ed83cd9ac
Gareth Tribello <gareth.tribello@gmail.com>
2022-08-11 19:44:33 +0100

    Fixed stupid bug in solution

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ for i in range(200) :
   # Add code to setup the numpy arrays called indices and average to generate the desired plot here.
   indices[i] = i+2
   myvar = np.random.uniform(0,1)
-  ssum, ssum2 = ssum + myvar, ssum + myvar*myvar 
+  ssum, ssum2 = ssum + myvar, ssum2 + myvar*myvar 
   mean = ssum / indices[i] 
   S2[i] = (indices[i] / (indices[i]-1))*( ssum2 / indices[i] - mean*mean )
 
